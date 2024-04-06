1a3c9f3dee8dd48a6e6efc410d1cf98893275720
joaoleandro <new.joaoleandro@gmail.com>
2024-04-06 17:10:02 -0300

    feat: Adicionado imagens ao projeto e adicionado os componentes de projeto na página principal

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             </p>
           </div>
 
-        <Contacts/>
+          <Contacts />
         </section>
 
         <div className="flex flex-col items-end lg:absolute relative top-0 right-0 w-full lg:w-1/2">
@@ -56,21 +56,94 @@ export default function Home() {
           </BoxScroll>
           <BoxScroll>
             <div className="mt-5">
-              <h2 className="text-2xl font-semibold text-emerald-300">Habilidades</h2>
-              <p className="text-slate-400/80">
-                Breve resumo...
-              </p>
-              <SkillsBar/>
+              <h2 className="text-2xl font-semibold text-emerald-300">
+                Habilidades
+              </h2>
+              <p className="text-slate-400/80">Breve resumo...</p>
+              <SkillsBar />
             </div>
           </BoxScroll>
           <BoxScroll>
-            <div className="mt-5 flex flex-col gap-2">
-              <h2 className="text-2xl font-semibold text-emerald-300">Projetos</h2>
-              <CardProjects imagem="imagemTeste.jpg" titulo="Titulo do projeto..." description="Descrição detalhada do projeto..."/>
+            <div className="mt-5 flex flex-col gap-8">
+              <h2 className="text-2xl font-semibold text-emerald-300">
+                Projetos
+              </h2>
+              <CardProjects
+                imagem="/assets/stack-burguer.jpg"
+                titulo="Stack Burguer"
+                description="A ideia desse projeto é consumir uma lista de itens em um restaurante, adicionar esses itens ao carrinho de compras e calcular o valor total dos pedidos."
+                url="https://stack-burguer-project.vercel.app/"
+              />
+              <CardProjects
+                imagem="/assets/star-wars.jpg"
+                titulo="Star Wars - Cronologia"
+                description="Projeto inspirado nos filmes star wars, mostrando a ordem correta para se assistir aos filmes."
+                url="https://joaoleandroo.github.io/page-star_wars-ordem_cronologica/"
+              />
+              <CardProjects
+                imagem="/assets/cristiano-ronaldo.jpg"
+                titulo="Cristiano Ronaldo"
+                description="Dentro desse projeto, é mostrada um pouco a história do lendário jogador de futebol Cristiano Ronaldo."
+                url="https://joaoleandroo.github.io/cristiano_ronaldo_page/"
+              />
+              <CardProjects
+                imagem="/assets/memory-game.jpg"
+                titulo="Jogo da memória"
+                description="Jogo da memória com cartas dos personagens do anime one piece, a cada rodada novas cartas são adicionadas aleatoriamente."
+                url="https://memory-game-six-rho.vercel.app/"
+              />
+              <CardProjects
+                imagem="/assets/game-pikachu.jpg"
+                titulo="Game Pikachu"
+                description="Joguinho do pikachu, visa fugir da pokebola. Para jogar, basta pressionar qualquer tecla."
+                url="https://joaoleandroo.github.io/game-pikachu/"
+              />
+              <CardProjects
+                imagem="/assets/buscador-cep.jpg"
+                titulo="Buscador CEP"
+                description="Dentro desse projeto é consumida uma API de CEP, basta digitar o CEP corretamente e será retornado um resultado."
+                url="https://buscador-cep-react-six.vercel.app/"
+              />
+              <CardProjects
+                imagem="/assets/one-piece-live-action.jpg"
+                titulo="One piece - Liveaction"
+                description="Projeto inspirado no lançamento do liveaction do anime One Piece."
+                url="https://one-piece-liveaction-7gtpptsyu-joaoleandroo.vercel.app/"
+              />
+              <CardProjects
+                imagem="/assets/luffy-gear-five.jpg"
+                titulo="Luffy Gear Five"
+                description="Projeto feito para demonstrar a grandiosidade da transformação do gear five, do personagem Luffy do anime One piece."
+                url="https://joaoleandroo.github.io/luffy-gear-five/"
+              />
+                            <CardProjects
+                imagem="/assets/save-websites.jpg"
+                titulo="Save websites"
+                description="Esse projeto foi inspirado no modo do google de salvar o link dos websites favoritos"
+                url="https://add-links-websites.vercel.app/"
+              />
+                            <CardProjects
+                imagem="/assets/lista-tarefas.jpg"
+                titulo="Lista de Tarefas"
+                description="Lista de tarefas com todos os requisitos de um CRUD, criar, ler, atualizar e deletar uma tarefa. Também foi utilizado o salvamento do localstorage."
+                url="https://joaoleandroo.github.io/task-list/"
+              />
+                            <CardProjects
+                imagem="/assets/prime-flix.jpg"
+                titulo="Prime Flix"
+                description="Dentro desse projeto é consumida uma API de filmes, onde é possível salvar os filmes favoritos e visualizar os filmes em cartaz no cinema."
+                url="https://jl-movies.netlify.app/"
+              />
+                                          <CardProjects
+                imagem="/assets/projeto-music.jpg"
+                titulo="Projeto Musica"
+                description="Projeto de musica landing page"
+                url="https://joaoleandroo.github.io/project-music/"
+              />
             </div>
           </BoxScroll>
 
-          <Footer/>
+          <Footer />
         </div>
       </div>
     </Container>
--- components/CardProjects.tsx
+++ components/CardProjects.tsx
@@ -1,20 +1,30 @@
 import Link from "next/link";
+import Image from "next/image";
 
 interface CardProjectsProps {
     imagem: string;
     titulo: string;
     description: string;
+    url: string;
 }
 
-const CardProjects: React.FC<CardProjectsProps> = ({ imagem, titulo, description }) => {
+const CardProjects: React.FC<CardProjectsProps> = ({ imagem, titulo, description, url }) => {
     return ( 
-        <section className="transition duration-200 rounded-lg bg-emerald-300/10 hover:bg-emerald-300/30 mb-20">
-            <Link href={"https://www.youtube.com/"} target="_blank" className=" w-full h-full">
+        <section className="transition duration-100 rounded-lg bg-emerald-200/10 hover:bg-emerald-300/30">
+            <Link href={url} target="_blank" className=" w-full h-full">
                 <div className="p-5 flex">
-                    <div className="w-[30%] border-r">{imagem}</div>
-                    <div className="w-[70%] ml-2">
-                        <h2>{titulo}</h2>
-                        <p>{description}</p>
+                    <div className="w-[30%]">
+                        <Image
+                            src={imagem}
+                            width={250}
+                            height={250}
+                            alt={titulo}
+                            className="rounded-lg"
+                        />
+                    </div>
+                    <div className="w-[70%] ml-4 flex flex-col gap-y-2">
+                        <h2 className="font-semibold text-xl">{titulo}</h2>
+                        <p className="text-slate-400/80">{description}</p>
                     </div>
                 </div>
             </Link>
