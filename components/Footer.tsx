2aa2b6661d6ff47f4e898f6bfb9b50642fd74f0a
joaoleandro <new.joaoleandro@gmail.com>
2024-04-06 20:44:01 -0300

    style: Footer concluido

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,9 @@
+import Contacts from "./Contacts";
+
 const Footer = () => {
     return ( 
-        <footer className="h-11 w-full flex items-center justify-center">
-            <p className="text-slate-400/60">
-                &copy; Joao Leandro
-            </p>
+        <footer className="w-full p-5 flex items-center justify-center mt-9 border-t border-slate-500 hover:bg-slate-700/60 transition duration-200">
+            <Contacts/>
         </footer>
      );
 }
